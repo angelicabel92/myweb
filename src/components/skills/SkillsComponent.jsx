5c3c65e835f81f29b2ea997d1250d68444603530
Angelica Beltran <angelica.beltran@selectra.com>
2019-10-09 18:07:00 +0200

    skill

--- src/components/skills/SkillsComponent.jsx
+++ src/components/skills/SkillsComponent.jsx
@@ -14,91 +14,77 @@ const SkillsComponent = () => {
                         <div className="col-md-6">
                             <div className="single-skill-content">
                                 <div className="barWrapper">
-                                    <span className="progressText">adobe photoshop</span>
+                                    <span className="progressText">HTML5 - CSS</span>
+                                    <div className="single-progress-txt">
+                                        <ProgressBar now={90} />
+                                        <h3>90%</h3>
+                                    </div>
+                                </div>
+                                <div className="barWrapper">
+                                    <span className="progressText">SCSS</span>
                                     <div className="single-progress-txt">
                                         <ProgressBar now={90} />
                                         <h3>90%</h3>	
                                     </div>
                                 </div>
                                 <div className="barWrapper">
-                                    <span className="progressText">adobe illustrator</span>
+                                    <span className="progressText">Flexbox-Grid</span>
                                     <div className="single-progress-txt">
-                                        <div className="progress">
-                                        <div className="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="10" aria-valuemax="100" style={{"width": "85%"}}>
-                                                
-                                        </div>
-                                        </div>
-                                        <h3>85%</h3>	
+                                        <ProgressBar now={70} />
+                                        <h3>70%</h3>	
                                     </div>
                                 </div>
                                 <div className="barWrapper">
-                                    <span className="progressText">adobe after effects</span>
+                                    <span className="progressText">Javascript | React | Vue </span>
                                     <div className="single-progress-txt">
-                                        <div className="progress ">
-                                        <div className="progress-bar" role="progressbar" aria-valuenow="97" aria-valuemin="10" aria-valuemax="100" style={{"width": "97%"}}>
-                                            
-                                        </div>
-                                        </div>
-                                        <h3>97%</h3>	
+                                        <ProgressBar now={60} />
+                                        <h3>60%</h3>	
                                     </div>
                                 </div>
-                                <div className="barWrapper">
-                                    <span className="progressText">sketch</span>
-                                    <div className="single-progress-txt">
-                                        <div className="progress ">
-                                        <div className="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="10" aria-valuemax="100" style={{"width": "90%"}}>
-                                                
-                                        </div>
-                                        </div>
-                                        <h3>90%</h3>	
-                                    </div>
+                            </div>
+                            <div className="barWrapper">
+                                <span className="progressText">Sketch</span>
+                                <div className="single-progress-txt">
+                                    <ProgressBar now={60} />
+                                    <h3>60%</h3>	
                                 </div>
                             </div>
                         </div>
                         <div className="col-md-6">
                             <div className="single-skill-content">
                                 <div className="barWrapper">
-                                    <span className="progressText">html 5</span>
+                                    <span className="progressText">InVision</span>
                                     <div className="single-progress-txt">
-                                        <div className="progress ">
-                                            <div className="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="10" aria-valuemax="100" style={{"width": "90%"}}>
-                                                
-                                            </div>
-                                        </div>
-                                        <h3>90%</h3>	
+                                        <ProgressBar now={80} />
+                                        <h3>80%</h3>	
                                     </div>
                                 </div>
                                 <div className="barWrapper">
-                                    <span className="progressText">css 3 animation</span>
+                                    <span className="progressText">Photoshop</span>
                                     <div className="single-progress-txt">
-                                        <div className="progress ">
-                                        <div className="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="10" aria-valuemax="100" style={{"width": "85%"}}>
-                                                
-                                        </div>
-                                        </div>
+                                        <ProgressBar now={85} />
                                         <h3>85%</h3>	
                                     </div>
                                 </div>
                                 <div className="barWrapper">
-                                    <span className="progressText">communication</span>
+                                    <span className="progressText">Ilustrator</span>
                                     <div className="single-progress-txt">
-                                        <div className="progress ">
-                                        <div className="progress-bar" role="progressbar" aria-valuenow="97" aria-valuemin="10" aria-valuemax="100" style={{"width": "97%"}}>
-                                            
-                                        </div>
-                                        </div>
-                                        <h3>97%</h3>	
+                                        <ProgressBar now={80} />
+                                        <h3>80%</h3>	
                                     </div>
                                 </div>
                                 <div className="barWrapper">
-                                    <span className="progressText"> creativity</span>
+                                    <span className="progressText">Git</span>
                                     <div className="single-progress-txt">
-                                        <div className="progress ">
-                                        <div className="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="10" aria-valuemax="100" style={{"width": "90%"}}>
-                                                
-                                        </div>
-                                        </div>
-                                        <h3>90%</h3>	
+                                        <ProgressBar now={70} />
+                                        <h3>70%</h3>	
+                                    </div>
+                                </div>
+                                <div className="barWrapper">
+                                    <span className="progressText">PHP - Laravel</span>
+                                    <div className="single-progress-txt">
+                                        <ProgressBar now={40} />
+                                        <h3>40%</h3>	
                                     </div>
                                 </div>
                             </div>
